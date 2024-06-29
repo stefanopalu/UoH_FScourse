66a3210a1f6c4f8c158706170cc879a55bba9b9d
Stefano Palumbo <stefanizzimail@hotmail.it>
2024-06-29 13:35:13 +0900

    unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Button = (props) => {
   return (
-    <button onClick={props.onClick}>
+    <button onClick={() => props.onClick()}>
       {props.text}
     </button>
   )
@@ -14,23 +14,54 @@ const Display = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+      <div>all {props.all}</div>
+      <div>average {(props.good*1+props.bad*-1)/props.all}</div>
+      <div>positive {(props.good/props.all)*100} %</div>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const goodClick = () => {
+    setAll(allClicks.concat(1))
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setAll(allClicks.concat(0))
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setAll(allClicks.concat(-1))
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text="good"/>
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
-      <Button onClick={() => setBad(bad + 1)} text="bad"/>
-      <h1>statistics</h1>
-      <Display text={"good"} counter={good}/>
-      <Display text={"neutral"} counter={neutral}/>
-      <Display text={"bad"} counter={bad}/>
-      <Display text={"all"} counter={good+neutral+bad}/>
+      <Button onClick={() => goodClick()} text="good"/>
+      <Button onClick={() => neutralClick()} text="neutral"/>
+      <Button onClick={() => badClick()} text="bad"/>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={allClicks.length}
+      />
     </div>
   )
 }
