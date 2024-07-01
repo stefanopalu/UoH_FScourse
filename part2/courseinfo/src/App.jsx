968b8a0660f0a5963bbe8900286017eb7ef42d6b
Stefano Palumbo <stefanizzimail@hotmail.it>
2024-07-01 14:57:38 +0900

    courseinfo step10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,45 +1,5 @@
-const Course = ({course}) =>{
-  console.log(course.name)
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+import { Course, Header, Part, Content } from './course';
 
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <p> total of {total} exercises</p>
-    </div>
-  )
-}
-
-const Header = ({name}) => {
-  return (
-  <h1>{name}</h1>
-  )
-}
-
-const Part = ({ part }) => {
-  console.log(part.name)
-  return (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  console.log(parts)
-  return (
-  <div>
-    <ul>
-      {parts.map(part => (
-        <li key={part.id}>
-          <Part part={part} />
-        </li>
-      ))}
-    </ul>
-  </div>
-  ) 
-}
 
 const App = () => {
   const courses = [
