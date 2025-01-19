90fbac7845e111fe446967cdc9b6825180aef50e
stefanopalu <stefanizzimail@hotmail.it>
2025-01-19 11:24:21 +0900

    Helper Functions and Unit Tests, step 3

--- part4/bloglistbackend/utils/list_helper.js
+++ part4/bloglistbackend/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blog) => {
   return blog.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blog) => {
+    return blog.reduce((best, current) => 
+        current.likes > best.likes ? current : best
+    )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
