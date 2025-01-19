5d1464b08e6dc02ec52dce5f6adb8e24fbfafd1a
stefanopalu <stefanizzimail@hotmail.it>
2025-01-19 14:37:19 +0900

    Blog List Tests, step 2

--- part4/bloglistbackend/tests/blog_api.test.js
+++ part4/bloglistbackend/tests/blog_api.test.js
@@ -2,15 +2,24 @@ const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const { assert } = require('console')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
+test.only('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test.only('blogs unique identifier', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    blogs.forEach (blog =>
+        assert(blog.id)
+    )
+  })
 
 after(async () => {
   await mongoose.connection.close()
