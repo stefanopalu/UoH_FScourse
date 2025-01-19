4d78b57e4aa7c49bff7ae06a3ef682c3e1889fa5
stefanopalu <stefanizzimail@hotmail.it>
2025-01-19 10:19:57 +0900

    bloglist step 2

---
+++ part4/bloglistbackend/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
