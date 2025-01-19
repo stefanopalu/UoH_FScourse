0b0f359e54a62138f923e25e56bb45082e5c7f59
stefanopalu <stefanizzimail@hotmail.it>
2025-01-19 15:15:35 +0900

    Blog List Tests, step 3

--- part4/bloglistbackend/tests/blog_api.test.js
+++ part4/bloglistbackend/tests/blog_api.test.js
@@ -1,10 +1,35 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const { assert } = require('console')
+const assert = require('node:assert')
 
 const api = supertest(app)
+
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+    {
+      title: 'title1',
+      author: 'author1',
+      url: "url1",
+      likes: 6,
+    },
+    {
+      title: 'title2',
+      author: 'author2',
+      url: "url2",
+      likes: 5,
+    },
+  ]
+  
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
 
 test.only('blogs are returned as json', async () => {
   await api
@@ -17,9 +42,32 @@ test.only('blogs unique identifier', async () => {
     const response = await api.get('/api/blogs')
     const blogs = response.body
     blogs.forEach (blog =>
-        assert(blog.id)
+        assert.ok(blog.id)
     )
   })
+
+test.only('a blog can be added ', async () => {
+  const newBlog = {
+    title: 'title',
+    author: 'author',
+    url: 'url',
+    likes: 0,
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  assert(titles.includes('title'))
+})
 
 after(async () => {
   await mongoose.connection.close()
