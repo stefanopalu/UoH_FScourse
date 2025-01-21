db95ce196ebd378bb7dcffd3eef9c935e8bfe34c
stefanopalu <stefanizzimail@hotmail.it>
2025-01-21 12:52:12 +0900

    Blog List Expansion, step 3

--- part4/bloglistbackend/app.js
+++ part4/bloglistbackend/app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
@@ -16,6 +19,8 @@ mongoose.connect(config.mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
