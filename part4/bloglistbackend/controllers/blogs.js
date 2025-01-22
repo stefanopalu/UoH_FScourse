001429491c638e70c0af9941f0d301aae7b62d88
stefanopalu <stefanizzimail@hotmail.it>
2025-01-22 11:24:46 +0900

    Blog List Expansion, step 5

--- part4/bloglistbackend/controllers/blogs.js
+++ part4/bloglistbackend/controllers/blogs.js
@@ -1,13 +1,19 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findOne({})
+  console.log(`name: ${user.name}`)
 
   if (!body.url || !body.title) {
     return response.status(400).json({error: 'Title or URL are missing'})
@@ -18,9 +24,14 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user.id,
   })
 
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
   response.status(201).json(savedBlog)
 })
 
