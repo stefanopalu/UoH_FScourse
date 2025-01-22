c7254e1a51abcd52f5d775dc2867920e84b13667
stefanopalu <stefanizzimail@hotmail.it>
2025-01-22 12:00:07 +0900

    Blog List Expansion, step 8

--- part4/bloglistbackend/app.js
+++ part4/bloglistbackend/app.js
@@ -22,6 +22,7 @@ mongoose.connect(config.mongoUrl)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
