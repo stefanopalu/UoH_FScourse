36158f667234a51c314cea81ec9f8fb09f0665ea
stefanopalu <stefanizzimail@hotmail.it>
2025-05-02 14:01:02 +0900

    WebBMI

---
+++ part9/firststeps/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+        const height = Number(req.query.height);
+        const weight = Number(req.query.weight);
+
+        if (!height || !weight || isNaN(height) || isNaN(weight)) {
+            res.status(400).json({ error: 'Malformatted parameters' });
+        }
+
+        const bmi = calculateBmi(height, weight);
+
+        res.json({
+            weight: weight,
+            height: height,
+            bmi: bmi,
+        });
+    });
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
