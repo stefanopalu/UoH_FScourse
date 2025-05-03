6ec6ccf76eaeb0eb4e474eca212a6025425dadc8
stefanopalu <stefanizzimail@hotmail.it>
2025-05-03 14:07:58 +0900

    step3

---
+++ part9/patientor/backend/src/services/diagnosesService.ts
@@ -0,0 +1,15 @@
+import diagnoses from '../../data/diagnoses';
+import { Diagnoses } from '../types';
+
+const getDiagnoses = (): Diagnoses[] => {
+  return diagnoses;
+};
+
+const addDiagnoses = () => {
+  return null;
+};
+
+export default {
+  getDiagnoses,
+  addDiagnoses
+};
