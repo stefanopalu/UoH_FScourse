5ca020a867b70e7e8abe7f9be24ed803f5ab508a
stefanopalu <stefanizzimail@hotmail.it>
2025-05-04 13:48:15 +0900

    9.19

--- part9/ilari/frontend/src/App.tsx
+++ part9/ilari/frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 import { Entry, NewEntry } from './types'
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import EntriesList from './components/EntriesList'
 import NewEntryForm from './components/NewEntryForm';
 
 function App() {
   const [entries, setEntries] = useState<Entry[]>([])
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   useEffect(() => {
     axios.get('http://localhost:3000/api/diaries').then(response => {
@@ -13,16 +14,24 @@ function App() {
     })
   }, []);
 
-  const handleNewEntry = (newEntry: NewEntry) => {
-    axios.post<Entry>('http://localhost:3000/api/diaries', newEntry )
-      .then(response => {
-        setEntries(entries.concat(response.data))
-      })
+  const handleNewEntry = async (newEntry: NewEntry) => {
+    try {
+      const response = await axios.post<Entry>('http://localhost:3000/api/diaries', newEntry )
+      setEntries(entries.concat(response.data));
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        const message = error.response.data?.message || "Something went wrong with the server"; 
+        setErrorMessage(message);
+      } else {
+        setErrorMessage("Unexpected error occurred"); // Catch unexpected errors
+      }
+    }
   }
 
   return (
     <div>
       <h1>Add new entry</h1>
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
       <NewEntryForm onEntryCreate={handleNewEntry}/>
 
       <h1>Diary entries</h1>
